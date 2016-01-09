b51bbd29cae77cc007f95b1098219fb1bb04a136
chungtv <chungtv@kaopiz.com>
2016-01-09 09:30:22 +0700

    Init project

---
+++ app/src/main/java/com/nafuto/example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nafuto.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nafuto.countdownview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
